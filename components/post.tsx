ec901e8cfef4069c45a6dcf8c2b6206d82d62fbf
BobBrian <rellrocker235@gmail.com>
2023-10-07 15:17:31 -0400

    Exapand Border Width

--- components/post.tsx
+++ components/post.tsx
@@ -32,14 +32,14 @@ export default function Post() {
     //m-auto controls the margin of the background area
     // https://css-tricks.com/almanac/properties/p/padding/ (Info on Padding)
     //max-w-md space-y-1 list-disc list-inside text-3xl mb-8
-    <div className="bg-slate-200  w-[calc(50%-2rem)]  max-w-5xl pt-16 pr-16 pb-8 pl-16  m-auto z-2"> 
+    <div className="bg-slate-200  w-[calc(80%-2rem)]  max-w-5xl pt-16 pr-16 pb-8 pl-16  m-auto z-2"> 
      {/* Main Project */}
      <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center underline">Hello Welcome to My Protfolio Site</h5> 
           </a>
      <div className="flex  pb-8">
            {/* w-3/4 is what controls the width of the table */}
-          <div className="w-3/4  p-6 bg-transparent  ">
+          <div className="p-6 bg-transparent  ">
           <p className="mb-3">I am full-stack developer which means I specialize in  building both The front end (the parts of a website a user sees and interacts with) and the back end (the behind-the-scenes data storage and processing) of a website which require different skill sets. </p>
           <h2 className="underline">Technology Specilizations</h2>
           <ul className="max-w-md space-y-1 list-disc list-inside  mb-8">
@@ -54,8 +54,9 @@ export default function Post() {
           </div>
       </div>
       {/* Container for Other Projects */}
+      {/* Container for Other Projects */}
       <div className="flex flex-wrap justify-center items-center">
-        <div className="w-3/4  p-6 bg-white border border-gray-200 rounded-lg ">
+        <div className="w-1/2  p-6 bg-white border border-gray-200 rounded-lg ">
           <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Sample Project A</h5> 
           </a>
@@ -65,7 +66,7 @@ export default function Post() {
           </a>
         </div>
 
-       <div className="w-3/4  p-6 bg-white border border-gray-200 rounded-lg ">
+       <div className="w-1/2  p-6 bg-white border border-gray-200 rounded-lg ">
           <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Sample Project A</h5> 
           </a>
@@ -75,7 +76,7 @@ export default function Post() {
           </a>
         </div>
 
-        <div className="w-3/4  p-6 bg-white border border-gray-200 rounded-lg ">
+        <div className="w-1/2  p-6 bg-white border border-gray-200 rounded-lg ">
           <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Sample Project A</h5> 
           </a>
@@ -85,7 +86,7 @@ export default function Post() {
           </a>
         </div>
 
-        <div className="w-3/4  p-6 bg-white border border-gray-200 rounded-lg ">
+        <div className="w-1/2  p-6 bg-white border border-gray-200 rounded-lg ">
           <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Sample Project A</h5> 
           </a>
