ebc1eb650777b9b9c98bfd4957f5cd2d9168584e
BobBrian <rellrocker235@gmail.com>
2023-10-31 05:55:34 -0400

    Yelp (Next 14 Update 3)

--- app/components/listTodo.tsx
+++ app/components/listTodo.tsx
@@ -11,9 +11,9 @@ export const ListTodo = ({todo,deleteTodo}: {todo: Todo; deleteTodo: (id: number
     <div  key={todo.id}>
         <h5 className="text-2xl font-bold text-gray-900">{todo.description}</h5>
         
-        <a className="mt-2 inline-flex items-center px-3 py-2 text-center text-white 
+        <button className="mt-2 inline-flex items-center px-3 py-2 text-center text-white 
         bg-red-500 rounded-lg rounded-lg hover:bg-red-900"
-         onClick={() => { startTransition(() => { deleteTodo(todo.id); });}} >Delete</a>
+         onClick={() => { startTransition(() => { deleteTodo(todo.id); });}} >Delete</button>
     </div>
   )
 }
--- app/resturant/[id]/page.tsx
+++ app/resturant/[id]/page.tsx
@@ -1,34 +1,41 @@
-import getResturant from '@/app/api/getResturant'
+
 import { Resturant } from '@/app/lib/definitions'
 import Link from 'next/link'
 import React from 'react'
+import * as PostApi from  "@/app/lib/actions"
+import { revalidatePath } from 'next/cache'
+import { ListReview } from '@/app/components/listReview'
 
 type Params  = {
     params: {
-        id: string
+        id: number
     }
 }
 
+type Resturants = Array <Resturant>
+
 export default async function ReviewDetails({params: {id}}: Params) {
 
-    const userData:Promise<Resturant> = getResturant(id)
+   const user : Resturants = await PostApi.GetResturant(id);
 
-    const user = await userData
-
+    const deleteReview = async (id: number) => {
+        "use server";
+        if(window.confirm('Are You Sure you want to delete this entry')){
+          await PostApi.DeleteReview(id);
+          revalidatePath("/");
+        } 
+    };
+    
     return (
         <>
-        <div key={user.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow items-center pt-16 pr-16 pb-8 pl-16  m-auto z-2">
-                <a>
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Review By {user.authorname}</h5>
-                </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">For {user.resturantname}</p>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{user.body}</p>
-                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium 
-                text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 
-                focus:outline-none focus:ring-blue-300">
-                    <Link href="/resturant">Back</Link>   
-                </a>
-        </div>
+            {user.map(userrest =>(
+                <div  className="max-w-sm bg-white border border-gray-200 
+                rounded-lg shadow items-center pt-16 pr-16 pb-8 pl-16  m-auto z-2">
+                    <ListReview key={userrest.id} userrest={userrest} deleteReview ={deleteReview } />
+                
+                </div>
+            ))};
+            
         </>
       )
 }
