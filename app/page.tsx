63846da8482af94f21ba002c3da0f5d27bb4d0d7
BobBrian <chibuikem.nwauche@dcmail.ca>
2024-01-24 18:18:55 -0500

    Correct Site Commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello to The new Chibuiekm Bryan Nwauche Dot Com , Its Time to get Serious
+       To The new Chibuiekm Bryan Nwauche Dot Com , Its Time to get Serious
     </main>
   );
 }
