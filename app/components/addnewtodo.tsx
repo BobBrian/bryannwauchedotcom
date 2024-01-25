1662c8e94f618e5e21638bf7ae88de0671dd0174
BobBrian <chibuikem.nwauche@dcmail.ca>
2024-01-25 11:07:47 -0500

    To-do List Commit

---
+++ app/components/addnewtodo.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React from 'react'
+import { useState, useTransition } from "react";
+
+export const AddNewTodo = ({ addNewTodo }: { addNewTodo: any }) => {
+
+    const [description,setDescription] = useState("")
+    const [isPending, startTransition] = useTransition();
+
+    const handleSubmit = (e:any) => {
+      e.preventDefault();
+      startTransition(() => {
+        addNewTodo(description);
+      });
+      
+    }
+
+  return (
+    <form  onSubmit={handleSubmit}>
+      <div>
+          <label className="block mb-2 text-lg font-medium text-slate-200 ">Todo Task</label>
+          <input value={description} className="bg-gray-50 border border-stone-600 text-gray-900 
+          rounded-lg focus:ring-blue-500 w-6/12" placeholder="Enter Task Here" onChange={(e) =>setDescription(e.target.value)} required/>
+      </div> 
+
+
+      <button className="mt-2 mb-2 text-center inline-flex items-center px-3 py-2 text-sm font-medium 
+        text-center text-white bg-blue-700 rounded-lg hover:bg-green-800">Add New Task</button>
+    </form>
+  )
+}
---
+++ app/todo/page.tsx
@@ -0,0 +1,54 @@
+
+import React from 'react'
+import { AddNewTodo } from '../components/addnewtodo';
+import * as PostApi from  "@/app/lib/actions"
+import { Todo } from '../lib/definitions';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { ListTodo } from '../components/listTodo'
+import Link from 'next/link'
+
+type Todos = Array<Todo>;
+
+export default async  function Todo() {
+  
+  const todos: Todos = await PostApi.GetAllTodo();
+
+  const postNewTodo = async (description: string) => {
+    "use server";
+    await PostApi.AddTodo(description);
+    revalidatePath('/todo');
+    redirect('/todo');
+    
+  };
+  
+  const deleteTodo = async (id: number) => {
+    "use server";
+    await PostApi.DeleteTodo(id);
+    revalidatePath('/todo');
+    redirect('/todo');
+    
+  };
+
+  return (
+   <main>
+      <h1 className="text-slate-200 text-center text-4xl mb-8">Chibuikem Nwauche</h1>
+      <h2 className="text-slate-200 text-center text-3xl mb-6">Todo List Application</h2>
+
+      <button className="mt-2 mb-2 inline-flex items-center px-3 py-2 text-sm font-medium 
+        text-center text-white bg-blue-700 rounded-lg hover:bg-red-800 ">
+        <Link href="/">Back to Home Page</Link>   
+      </button>
+
+      <AddNewTodo addNewTodo={postNewTodo}/>
+
+      <div>
+        {todos.map(todo =>(
+         <div key={todo.id}>
+          <ListTodo key={todo.id} todo={todo} deleteTodo={deleteTodo}/>
+         </div>
+        ))}
+      </div>
+   </main>
+  )
+}
